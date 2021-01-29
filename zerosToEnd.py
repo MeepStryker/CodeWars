1ccff802abd5dc4389173db47f0823f11657e827
MeepStryker <42586419+MeepStryker@users.noreply.github.com>
2021-01-28 19:41:58 -0500

    Create zerosToEnd.py

---
+++ zerosToEnd.py
@@ -0,0 +1,21 @@
+'''
+Moving Zeros to the End - 5 kyu
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+move_zeros([False,1,0,1,2,0,1,3,"a"]) # returns[False,1,1,2,1,3,"a",0,0]
+MeepStryker
+'''
+def move_zeros(array):
+    return None
+    
+print(move_zeros([1,2,0,1,0,1,0,3,0,1]),[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
+print(move_zeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9]),[9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
+print(move_zeros(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9]),["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
+print(move_zeros(["a",0,0,"b",None,"c","d",0,1,False,0,1,0,3,[],0,1,9,0,0,{},0,0,9]),["a","b",None,"c","d",1,False,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0])
+print(move_zeros([0,1,None,2,False,1,0]),[1,None,2,False,1,0,0])
+print(move_zeros(["a","b"]),["a","b"])
+print(move_zeros(["a"]),["a"])
+print(move_zeros([0,0]),[0,0])
+print(move_zeros([0]),[0])
+print(move_zeros([False]),[False])
+print(move_zeros([]),[])
